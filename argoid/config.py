4bff706f04cb60fb3abef9abbd6cfb509c471d1e
Wim Muskee <wimmuskee@gmail.com>
2018-01-28 19:05:41 +0100

    configurable offset for each interaction

--- argoid/config.py
+++ argoid/config.py
@@ -10,9 +10,9 @@ class Configuration:
 
         # also validate
 
-        # set initial and recurring time for all interactions
+        # set initial time for all interactions
         for interaction_name in self.config["interactions"]:
-            self.config["interactions"][interaction_name]["timestamp"] = round(time())
+            self.config["interactions"][interaction_name]["timestamp"] = round(time()) + self.config["interactions"][interaction_name]["offset"]
 
     def getActionTime(self,interaction):
         return self.config["interactions"][interaction]["timestamp"] + self.config["interactions"][interaction]["recurring"]
